8d9bccd062ffb1f4ef40369b2408d5329ce70f1f
KimByungGab <40555156+KimByungGab@users.noreply.github.com>
2024-01-16 04:38:47 +0900

    2단계 IOCP 실습
    2단계 IOCP 실습

---
+++ 02/main.cpp
@@ -0,0 +1,29 @@
+﻿#include "NetworkCore.h"
+
+int main()
+{
+	NetworkCore networkCore;
+
+	// 소켓 초기화
+	networkCore.InitSocket();
+
+	// 소켓과 서버 주소 연결 후 등록
+	networkCore.BindAndListen();
+
+	// 서버 시작
+	networkCore.StartServer();
+
+	cout << "종료: quit" << endl;
+	while (true)
+	{
+		string input;
+		getline(cin, input);
+
+		if (input == "quit")
+			break;
+	}
+
+	networkCore.DestoryThread();
+
+	return 0;
+}

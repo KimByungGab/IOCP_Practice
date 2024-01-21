8723ab95c77c9b0901cf5961df9217ca52e1b947
KimByungGab <40555156+KimByungGab@users.noreply.github.com>
2024-01-21 18:15:07 +0900

    7단계 main 파일
    7단계 main 파일

---
+++ 07/main.cpp
@@ -0,0 +1,29 @@
+﻿#include "NetworkServer.h"
+
+int main()
+{
+	NetworkServer server;
+
+	// 소켓 초기화
+	server.InitSocket();
+
+	// 소켓과 서버 주소 연결 후 등록
+	server.BindAndListen();
+
+	// 서버 시작
+	server.Run();
+
+	cout << "종료: quit" << endl;
+	while (true)
+	{
+		string input;
+		getline(cin, input);
+
+		if (input == "quit")
+			break;
+	}
+
+	server.End();
+
+	return 0;
+}
